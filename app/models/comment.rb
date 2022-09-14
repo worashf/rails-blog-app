352db03f82f744220b41adc680a697ccd3001558
worashf <uppert83@gmail.com>
2022-09-15 02:29:13 +0300

    Atach method after save

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+  after_save :update_comment_count
   def update_comment_count
     post.upadate(comments_counter: post.comments.count)
   end
