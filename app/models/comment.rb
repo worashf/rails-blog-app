bf015cecda73f955cda020602093731b96d807c9
worashf <uppert83@gmail.com>
2022-09-15 01:53:02 +0300

    Create relationship and add update_comment_counter method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+  def update_comment_count
+    post.upadate(comments_counter: post.comments.count)
+  end
 end
