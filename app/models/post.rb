8d34755f8b768edd7d985b449f5c8a83f85918bf
worashf <uppert83@gmail.com>
2022-09-15 01:54:37 +0300

    Addassociation and methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  def update_posts_counter
+    author.upadate(posts_counter: author.posts.count)
+  end
+
+  def recent_five_comments
+    comments.includes(:post).order(created_at: :desc).limit(5)
+  end
 end
