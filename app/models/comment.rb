a4884908d07de57bf7f7edfb74d2380c989bcbb3
worashf <uppert83@gmail.com>
2022-09-16 15:53:54 +0300

    Add validation for text

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
   after_save :update_comment_count
+  validates :text, presence: true
 
   private
 
   def update_comment_count
-    post.upadate(comments_counter: post.comments.count)
+    post.update(comments_counter: post.comments.count)
   end
 end
