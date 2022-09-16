b86d437b83dc9e020878ee6abe3ddfe75d904d07
worashf <uppert83@gmail.com>
2022-09-16 15:54:18 +0300

    Add  validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,15 +2,18 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :likes_counter, :comments_counter, numericality: { only_integer: true,
+                                                               greater_than_or_equal_to: 0 }
   after_save :update_posts_counter
+
+  def recent_five_comments
+    comments.includes(:post).order(created_at: :desc).limit(5)
+  end
 
   private
 
   def update_posts_counter
-    author.upadate(posts_counter: author.posts.count)
-  end
-
-  def recent_five_comments
-    comments.includes(:post).order(created_at: :desc).limit(5)
+    author.update(posts_counter: author.posts.count)
   end
 end
