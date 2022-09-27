249d16b9fc078bda26cf2c4b9f6d05fcd4341077
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-27 20:36:45 +0100

    Add post show test

--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -22,7 +22,18 @@ RSpec.describe "Posts", type: :feature do
         it "Should show the number fo comments given for the post" do 
             expect(page).to have_content 'Comments: 3'
         end
-
+        it "Should show the numbers of like for post"  do
+            expect(@posts.first.likes_counter).to be 0
+         end
+         it  "Should show the post text" do
+            expect(page).to have_content @posts.first.text
+         end
+         it "Should show the user name of the comentor" do
+          @comments.each {  |comment| expect(page).to have_content comment.author.name }
+         end
+         it "Should show the comment text " do
+            @comments.each {  |comment| expect(page).to have_content comment.text }
+           end
      
     end
 
