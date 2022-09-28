580b8eb6f6f5d59ad2b989e7e8610acd73f179a4
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-28 23:53:50 +0100

    Fix linter issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,22 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :null_session
-    before_action :authenticate_user!
-    before_action :update_allowed_parameters, if: :devise_controller?
+  protect_from_forgery with: :null_session
+  before_action :authenticate_user!
+  before_action :update_allowed_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-    def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio,:photo, :email, :password, :password_confirmation) }
-      devise_parameter_sanitizer.permit(:account_update) do |u|
-        u.permit(:name, :surname, :email, :password, :current_password)
-      end
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
     end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :surname, :email, :password, :current_password)
+    end
+  end
 
-    private
+  private
 
-    def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
-    end
+  end
 end
