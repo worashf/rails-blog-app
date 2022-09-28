96ea4c7774eac22a120c2f1ec2d5d7e13c6af00b
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-28 23:50:05 +0100

    Add the application controller before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
-  
- 
+    protect_from_forgery with: :null_session
+    before_action :authenticate_user!
+    before_action :update_allowed_parameters, if: :devise_controller?
+
+    protected
+
+    def update_allowed_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio,:photo, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:name, :surname, :email, :password, :current_password)
+      end
+    end
+
+    private
+
+    def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+    end
 end
