0ef3dbb56240409a8f8ef92a71036baa7a266a40
worashf <uppert83@gmail.com>
2022-09-29 21:33:15 +0300

    Permit role params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :null_session
   before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
@@ -7,10 +8,10 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
+      u.permit(:name, :bio, :photo, :email,:role,:password, :password_confirmation)
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :surname, :email, :password, :current_password)
+      u.permit(:name, :bio,:photo, :email, :role, :password, :current_password)
     end
   end
 
