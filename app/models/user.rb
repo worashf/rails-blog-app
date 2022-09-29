aaada150b7bf006baf68633da256b2af25c0d205
worashf <uppert83@gmail.com>
2022-09-29 22:58:29 +0300

    Reove comments eager loaging

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   def recent_three_posts
-    posts.includes(:author, comments: [:author]).order(created_at: :desc).limit(3)
+    posts.includes(:author).order(created_at: :desc).limit(3)
   end
 
   def admin?
