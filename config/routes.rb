067249be6253c834785af094ee72da2935c1831c
worashf <uppert83@gmail.com>
2022-09-29 22:59:21 +0300

    Add destroy post route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do 
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do 
       resources :comments, only: [:create,:destroy]
       resources :likes, only: [:create, :destroy]
     end
