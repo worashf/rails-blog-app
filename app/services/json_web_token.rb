87017ad87c861159bea8b590ede84bada647a005
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-30 23:56:32 +0100

    Fix linter issue in json web token app

--- app/services/json_web_token.rb
+++ app/services/json_web_token.rb
@@ -1,21 +1,19 @@
-
 class JsonWebToken
-    HMAC_SECRET = Rails.application.secrets.secret_key_base
-    ALGORITHM_TYPE = 'HS256'.freeze
+  HMAC_SECRET = Rails.application.secrets.secret_key_base
+  ALGORITHM_TYPE = 'HS256'.freeze
 
-    def self.encode(user_id)
-        exp = 24.hours.from_now.to_i
-        payload = { user_id: user_id, exp: exp }
-        JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
-    end
-  
-    def self.decode(token)
-        body = JWT.decode(token, JWT_SECRET)[0]
-        HashWithIndifferentAccess.new body
-      rescue JWT::ExpiredSignature, JWT::VerificationError => e
-        raise ExceptionHandler::ExpiredSignature, e.message
-      rescue JWT::DecodeError, JWT::VerificationError => e
-        raise ExceptionHandler::DecodeError, e.message
-      end
+  def self.encode(user_id)
+    exp = 24.hours.from_now.to_i
+    payload = { user_id:, exp: }
+    JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
+  end
 
-end 
+  def self.decode(token)
+    body = JWT.decode(token, JWT_SECRET)[0]
+    HashWithIndifferentAccess.new body
+  rescue JWT::ExpiredSignature, JWT::VerificationError => e
+    raise ExceptionHandler::ExpiredSignature, e.message
+  rescue JWT::DecodeError, JWT::VerificationError => e
+    raise ExceptionHandler::DecodeError, e.message
+  end
+end
