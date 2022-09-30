3e2253bd7a87e7b70c8ae17331c6a2d112391b85
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-30 23:55:31 +0100

    Fix linter issue in posts controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,24 +1,24 @@
 class Api::PostsController < ApplicationController
-    before_action :authorize_request
-    before_action :user
-  
-    def index
-      @posts = Post.includes(:author).where(author: params[:user_id])
-      render json: @posts
-    end
-  
-    def show
-      post = @user.posts.find_by_id!(params[:id])
-      render json: post
-    rescue ActiveRecord::RecordNotFound
-      render json: { errors: 'Post not found' }, status: :not_found
-    end
-  
-    private
-  
-    def user
-      @user ||= User.find_by_id!(params[:user_id])
-    rescue ActiveRecord::RecordNotFound
-      render json: { errors: 'User not found' }, status: :not_found
-    end
+  before_action :authorize_request
+  before_action :user
+
+  def index
+    @posts = Post.includes(:author).where(author: params[:user_id])
+    render json: @posts
   end
+
+  def show
+    post = @user.posts.find_by_id!(params[:id])
+    render json: post
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'Post not found' }, status: :not_found
+  end
+
+  private
+
+  def user
+    @user ||= User.find_by_id!(params[:user_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'User not found' }, status: :not_found
+  end
+end
