88b0ce95f26a5e240764aeb8b09d8af3c30e2890
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-30 23:54:23 +0100

    Fix linter issue in authentication controller

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,17 +1,15 @@
 require './services/json_web_token'
-class Authentication  < ApplicationController
+class Authentication < ApplicationController
   skip_before_action :verify_authenticity_tokens
-    def login
-        @user = User.find_by_email(params[:email])
-        if @user&.authenticate(params[:password])
-          token = JsonWebToken.encode(user_id: @user.id)
-          time = Time.now + 24.hours.to_i
-          render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
-                         username: @user.username }, status: :ok
-        else
-          render json: { error: 'unauthorized' }, status: :unauthorized
-        end
-      end
-      
-  
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user&.authenticate(params[:password])
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { token:, exp: time.strftime('%m-%d-%Y %H:%M'),
+                     username: @user.username }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+  end
 end
