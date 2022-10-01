a512bd35c95d491806473dc872bc7076f8a984e8
worashf <uppert83@gmail.com>
2022-10-01 08:18:27 +0300

    fix typo

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,13 +1,11 @@
 require './services/json_web_token'
 class Authentication < ApplicationController
-  skip_before_action :verify_authenticity_tokens
+  skip_before_action :verify_authenticity_token
   def login
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
-      time = Time.now + 24.hours.to_i
-      render json: { token:, exp: time.strftime('%m-%d-%Y %H:%M'),
-                     username: @user.username }, status: :ok
+      render json: { token: }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
